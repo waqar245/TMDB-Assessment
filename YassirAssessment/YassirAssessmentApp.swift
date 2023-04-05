a2b5f7cec83c832e371b55a6a9d87f18e319e0b3
Waqar Ahmed <waqar.ahmed@yababa.com>
2023-04-05 13:12:34 +0500

    Initial Commit

---
+++ YassirAssessment/YassirAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirAssessmentApp.swift
+//  YassirAssessment
+//
+//  Created by Waqar Ahmed on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct YassirAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
